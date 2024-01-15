1a9819f6ac99b855dd7754a3b51e6eb020a42937
nopal778 <nopalmajenang@gmail.com>
2024-01-15 15:33:27 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <Text>I'm Nopal, Student in SMK Telkom Purwokerto</Text>
       <StatusBar style="auto" />
     </View>
   );
